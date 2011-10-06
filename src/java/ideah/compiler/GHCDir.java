1146d53af7b3128730195bf75c07cd3e65ffdbfb
amaurremi <amaurremi@9f1b87a4-9290-56fd-1e0b-2b6658918edd>
2011-10-06 15:20:30 +0000

    catch NumberFormatException (Issue #4)

--- src/java/ideah/compiler/GHCDir.java
+++ src/java/ideah/compiler/GHCDir.java
@@ -3,14 +3,21 @@ package ideah.compiler;
 final class GHCDir {
 
     final String name;
-    final Integer[] version;
+    Integer[] version;
 
     GHCDir(String name) {
         this.name = name;
         String[] versionStr = name.split("[^0-9]");
         version = new Integer[versionStr.length];
         for (int i = 0; i < versionStr.length; i++) {
-            version[i] = Integer.parseInt(versionStr[i]);
+            try {
+                version[i] = Integer.parseInt(versionStr[i]);
+            } catch (NumberFormatException e) {
+                Integer[] temp = new Integer[i];
+                System.arraycopy(version, 0, temp, 0, i);
+                version = temp;
+                return;
+            }
         }
     }
 }
