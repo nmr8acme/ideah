ce52864c7dd23ddb97d565013562483035021576
sobolev.ov@gmail.com <sobolev.ov@gmail.com@9f1b87a4-9290-56fd-1e0b-2b6658918edd>
2011-11-01 08:32:42 +0000

    Simplified GetDeclPos

--- src/haskell/ask_ghc/GetDeclPos.hs
+++ src/haskell/ask_ghc/GetDeclPos.hs
@@ -21,7 +21,7 @@ extractPos line col var loc _ = liftIO $
     when (isGoodSrcSpan loc && srcSpanStartLine loc == line && srcSpanStartCol loc == col) $ do
         let loc' = nameSrcLoc $ idName var
         putStrLn $ locStr loc'
-        print $ srcLocFile $ nameSrcLoc $ varName var
+        print $ srcLocFile loc'
         exitSuccess
 
 doExtractPos :: Int -> Int -> TypecheckedModule -> Ghc ()
