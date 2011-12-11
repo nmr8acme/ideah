494db28ef25befe93bdc8e8dc398af7a951184de
sobolev.ov@gmail.com <sobolev.ov@gmail.com@9f1b87a4-9290-56fd-1e0b-2b6658918edd>
2011-12-11 17:06:32 +0000

    Checking ask_haddock even if ask_ghc is up to date

--- src/java/ideah/util/CompilerLocation.java
+++ src/java/ideah/util/CompilerLocation.java
@@ -1,19 +1,22 @@
 package ideah.util;
 
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.progress.ProgressIndicator;
 import com.intellij.openapi.progress.Task;
 import com.intellij.openapi.project.Project;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
-import java.io.IOException;
 
 public final class CompilerLocation {
 
     private static final Logger LOG = Logger.getInstance("ideah.util.CompilerLocation");
     private static final String MAIN_FILE = "ask_ghc";
+
+    private static boolean askHaddockChecked = false;
 
     public final String exe;
     public final String libPath;
@@ -27,12 +30,17 @@ public final class CompilerLocation {
         AskUtil ask = AskUtil.get(module, MAIN_FILE);
         if (ask == null)
             return null;
+        if (!askHaddockChecked) {
+            compileAskHaddock(module);
+            askHaddockChecked = true;
+        }
         String ghcLib = ask.getLibDir();
         if (ghcLib == null)
             return null;
         try {
             if (ask.needRecompile()) {
-                if (!compileHs(module, ask))
+                // todo: show foreground dialog
+                if (!ask.compileHs())
                     return null;
             }
             File exe = ask.getExe();
@@ -47,9 +55,9 @@ public final class CompilerLocation {
         }
     }
 
-    private static boolean compileHs(final Module module, AskUtil ask) throws IOException, InterruptedException {
+    private static void compileAskHaddock(final Module module) {
         Project project = module.getProject();
-        Task haddockBackgroundTask = new Task.Backgroundable(project, "Installing Haddock 2.9.2 if missing", true) {
+        final Task haddockBackgroundTask = new Task.Backgroundable(project, "Installing Haddock 2.9.2 if missing", true) {
             public void run(ProgressIndicator indicator) {
                 indicator.setText("Checking Haddock installation...");
                 indicator.setFraction(0.0);
@@ -57,7 +65,10 @@ public final class CompilerLocation {
                 indicator.setFraction(1.0);
             }
         }.setCancelText("Stop Haddock installation");
-        haddockBackgroundTask.queue();
-        return ask.compileHs();
+        ApplicationManager.getApplication().invokeLater(new Runnable() {
+            public void run() {
+                haddockBackgroundTask.queue();
+            }
+        });
     }
 }
